628e9700cac55b9be0277bfccde12d9fecf1e6f4
Chris Dunphy <chris@megaparsec.ca>
2026-03-11 21:14:18 -0600

    Add some cool keybinds to mini.ai

--- lua/amn/plugins/mini.lua
+++ lua/amn/plugins/mini.lua
@@ -7,21 +7,38 @@ return {
 
     -- The plugin and their config functions live here...
     local mini_plugins = {
-      ["mini.icons"] = {},
-      ["mini.surround"] = {},
-      ["mini.comment"] = {},
-      ["mini.statusline"] = {},
-      ["mini.ai"] = {},
-      ["mini.diff"] = {},
+      ["mini.icons"] = function(m)
+        m.setup()
+      end,
+      ["mini.surround"] = function(m)
+        m.setup()
+      end,
+      ["mini.comment"] = function(m)
+        m.setup()
+      end,
+      ["mini.statusline"] = function(m)
+        m.setup()
+      end,
+      ["mini.diff"] = function(m)
+        m.setup()
+      end,
+      ["mini.ai"] = function(m)
+        m.setup({
+          custom_textobjects = {
+            f = m.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+            c = m.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+          },
+        })
+      end,
     }
 
     -- Iterate over each mini plugin and run setup() on it
-    for plugin_str, plugin_conf in pairs(mini_plugins) do
+    for plugin_str, plugin_fn in pairs(mini_plugins) do
       local plugin = utils.do_import(plugin_str)
       if not plugin then
         return
       end
-      plugin.setup(plugin_conf)
+      plugin_fn(plugin)
     end
   end,
 }
