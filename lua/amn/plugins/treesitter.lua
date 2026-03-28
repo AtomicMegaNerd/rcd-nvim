12c61c00fecd99392c93083d82739c20417548c1
Chris Dunphy <chris@megaparsec.ca>
2026-03-28 17:00:19 -0600

    Update treesitter

--- lua/amn/plugins/treesitter.lua
+++ lua/amn/plugins/treesitter.lua
@@ -3,26 +3,39 @@ return {
   branch = "main",
   lazy = false,
   build = ":TSUpdate",
+
   config = function()
-    require("nvim-treesitter")
-      .install({
-        "bash",
-        "go",
-        "json",
-        "lua",
-        "markdown",
-        "python",
-        "sql",
-        "vim",
-        "yaml",
-        "toml",
-        "nix",
-        "diff",
-        "gomod",
-        "fish",
-        "html",
-        "css",
+    local langs = {
+      "bash",
+      "dockerfile",
+      "go",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "sql",
+      "vim",
+      "yaml",
+      "toml",
+      "nix",
+      "diff",
+      "gomod",
+      "fish",
+      "html",
+      "css",
+    }
+
+    require("nvim-treesitter").install(langs):wait(300000)
+
+    local trs_grp = vim.api.nvim_create_augroup("Treesitter", { clear = true })
+    for _, lang in ipairs(langs) do
+      vim.api.nvim_create_autocmd("FileType", {
+        group = trs_grp,
+        pattern = lang,
+        callback = function()
+          vim.treesitter.start()
+        end,
       })
-      :wait(300000)
+    end
   end,
 }
