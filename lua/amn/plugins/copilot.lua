ee281540b90e30422f62d5eae21c0ae6c2489d8a
Chris Dunphy <chris@megaparsec.ca>
2026-03-28 13:56:41 -0600

    co-pilot keybindings

--- lua/amn/plugins/copilot.lua
+++ lua/amn/plugins/copilot.lua
@@ -8,8 +8,8 @@ return {
       auto_trigger = false,
       keymap = {
         accept = "<C-y>",
-        next = "<C-y>",
-        prev = false,
+        next = "<C-n>",
+        prev = "<C-p>",
         dismiss = "<Esc>",
       },
     },
