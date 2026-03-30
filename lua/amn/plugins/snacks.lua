c5c923cb7f4c928f4088bbc0c53fe980ce6f99ae
Chris Dunphy <chris.dunphy@fglsports.com>
2026-03-30 14:23:14 -0600

    Fix snacks.nvim config

--- lua/amn/plugins/snacks.lua
+++ lua/amn/plugins/snacks.lua
@@ -48,42 +48,122 @@ return {
     -- file rename on disk with LSP import updates
     rename = { enabled = true },
   },
-  config = function(_, opts)
-    local snacks = require("snacks")
-    snacks.setup(opts)
-
-    if opts.input and opts.input.enabled then
-      vim.ui.input = snacks.input.input
-    end
-
-    local map = vim.keymap.set
-    local p = snacks.picker
-
+  keys = {
     -- Files
-    map("n", "<leader>ff", p.files, { desc = "[F]ind [F]iles" })
-    map("n", "<leader>fb", p.buffers, { desc = "[F]ind [B]uffers" })
-    map("n", "<leader>fl", p.grep, { desc = "[F]ind [L]ive grep" })
-    map("n", "<leader>fh", p.help, { desc = "[F]ind [H]elp tags" })
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "[F]ind [F]iles",
+    },
+    {
+      "<leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "[F]ind [B]uffers",
+    },
+    {
+      "<leader>fl",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "[F]ind [L]ive grep",
+    },
+    {
+      "<leader>fh",
+      function()
+        Snacks.picker.help()
+      end,
+      desc = "[F]ind [H]elp tags",
+    },
 
     -- LSP
-    map("n", "<leader>fs", p.lsp_symbols, { desc = "[F]ind [S]ymbols" })
-    map("n", "<leader>fw", p.lsp_workspace_symbols, { desc = "[F]ind [W]orkspace symbols" })
-    map("n", "<leader>fr", p.lsp_references, { desc = "[F]ind [R]eferences" })
+    {
+      "<leader>fs",
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = "[F]ind [S]ymbols",
+    },
+    {
+      "<leader>fw",
+      function()
+        Snacks.picker.lsp_workspace_symbols()
+      end,
+      desc = "[F]ind [W]orkspace symbols",
+    },
+    {
+      "<leader>fr",
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      desc = "[F]ind [R]eferences",
+    },
 
     -- Diagnostics
-    map("n", "<leader>fd", p.diagnostics_buffer, { desc = "[F]ind Document [d]iagnostics" })
-    map("n", "<leader>fD", p.diagnostics, { desc = "[F]ind Workspace [D]iagnostics" })
+    {
+      "<leader>fd",
+      function()
+        Snacks.picker.diagnostics_buffer()
+      end,
+      desc = "[F]ind Document [d]iagnostics",
+    },
+    {
+      "<leader>fD",
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = "[F]ind Workspace [D]iagnostics",
+    },
 
     -- Quickfix / keymaps
-    map("n", "<leader>fq", p.qflist, { desc = "[F]ind [Q]uickfix" })
-    map("n", "<leader>fk", p.keymaps, { desc = "[F]ind [K]eymaps" })
+    {
+      "<leader>fq",
+      function()
+        Snacks.picker.qflist()
+      end,
+      desc = "[F]ind [Q]uickfix",
+    },
+    {
+      "<leader>fk",
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = "[F]ind [K]eymaps",
+    },
 
     -- Git
-    map("n", "<leader>fgf", p.git_files, { desc = "[F]ind [G]it files" })
-    map("n", "<leader>fgc", p.git_log, { desc = "[F]ind [G]it [C]ommits" })
-    map("n", "<leader>fgb", p.git_branches, { desc = "[F]ind [G]it [B]ranches" })
+    {
+      "<leader>fgf",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "[F]ind [G]it [F]iles",
+    },
+    {
+      "<leader>fgc",
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = "[F]ind [G]it [C]ommits",
+    },
+    {
+      "<leader>fgb",
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = "[F]ind [G]it [B]ranches",
+    },
 
     -- Notifications
-    map("n", "<leader>fn", p.notifications, { desc = "[F]ind past [N]otifications" })
-  end,
+    {
+      "<leader>fn",
+      function()
+        Snacks.picker.notifications()
+      end,
+      desc = "[F]ind past [N]otifications",
+    },
+  },
 }
