09c483e97b2dba90f3bde70dcdda47d807614331
Chris Dunphy <chris@megaparsec.ca>
2026-03-31 07:33:56 -0600

    Fix silly bug

--- lua/amn/plugins/lsp.lua
+++ lua/amn/plugins/lsp.lua
@@ -122,48 +122,40 @@ return {
 
         local buf = args.buf
 
-        -- Keymaps: only set on first client to attach to this buffer
-        if #vim.lsp.get_clients({ bufnr = buf }) == 1 then
-          -- General LSP keymaps
-          vim.keymap.set(
-            "n",
-            "<leader>rn",
-            vim.lsp.buf.rename,
-            { buffer = buf, desc = "[R]e[n]ame" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>ca",
-            vim.lsp.buf.code_action,
-            { buffer = buf, desc = "[C]ode [A]ction" }
-          )
-          vim.keymap.set(
-            "n",
-            "<C-k>",
-            vim.lsp.buf.signature_help,
-            { buffer = buf, desc = "Signature Documentation" }
-          )
-          vim.keymap.set("n", "<leader>ti", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
-          end, { buffer = buf, desc = "[T]oggle [I]nlay Hint" })
+        -- General LSP keymaps
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = buf, desc = "[R]e[n]ame" })
+        vim.keymap.set(
+          "n",
+          "<leader>ca",
+          vim.lsp.buf.code_action,
+          { buffer = buf, desc = "[C]ode [A]ction" }
+        )
+        vim.keymap.set(
+          "n",
+          "<C-k>",
+          vim.lsp.buf.signature_help,
+          { buffer = buf, desc = "Signature Documentation" }
+        )
+        vim.keymap.set("n", "<leader>ti", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+        end, { buffer = buf, desc = "[T]oggle [I]nlay Hint" })
 
-          -- Picker-backed LSP navigation (overrides gr* defaults)
-          vim.keymap.set("n", "gd", function()
-            Snacks.picker.lsp_definitions()
-          end, { buffer = buf, desc = "[G]oto [D]efinition" })
-          vim.keymap.set("n", "gl", function()
-            Snacks.picker.lsp_declarations()
-          end, { buffer = buf, desc = "[G]oto Dec[l]aration" })
-          vim.keymap.set("n", "grr", function()
-            Snacks.picker.lsp_references()
-          end, { buffer = buf, desc = "[G]oto [R]eferences" })
-          vim.keymap.set("n", "gri", function()
-            Snacks.picker.lsp_implementations()
-          end, { buffer = buf, desc = "[G]oto [I]mplementation" })
-          vim.keymap.set("n", "grt", function()
-            Snacks.picker.lsp_type_definitions()
-          end, { buffer = buf, desc = "[G]oto [T]ype Definition" })
-        end
+        -- Picker-backed LSP navigation (overrides gr* defaults)
+        vim.keymap.set("n", "gd", function()
+          Snacks.picker.lsp_definitions()
+        end, { buffer = buf, desc = "[G]oto [D]efinition" })
+        vim.keymap.set("n", "gl", function()
+          Snacks.picker.lsp_declarations()
+        end, { buffer = buf, desc = "[G]oto Dec[l]aration" })
+        vim.keymap.set("n", "grr", function()
+          Snacks.picker.lsp_references()
+        end, { buffer = buf, desc = "[G]oto [R]eferences" })
+        vim.keymap.set("n", "gri", function()
+          Snacks.picker.lsp_implementations()
+        end, { buffer = buf, desc = "[G]oto [I]mplementation" })
+        vim.keymap.set("n", "grt", function()
+          Snacks.picker.lsp_type_definitions()
+        end, { buffer = buf, desc = "[G]oto [T]ype Definition" })
 
         -- Disable completion for HTML since Emmet provides it
         if client.name == "html" then
