883013e0fa44ae88bae5ceb59c36dca51777a610
Chris Dunphy <chris.dunphy@fglsports.com>
2026-03-31 08:55:03 -0600

    Update lua_ls setup

--- lua/amn/lsp/lua_ls.lua
+++ lua/amn/lsp/lua_ls.lua
@@ -1,8 +1,24 @@
 return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  -- NOTE: These will be merged with the configuration file.
   settings = {
     Lua = {
-      diagnostics = {
-        globals = { "vim", "Snacks" },
+      -- Using stylua for formatting.
+      format = { enable = false },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+      },
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
       },
     },
   },
