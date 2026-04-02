ba99234578ebd07677207afc69c10892b19569d7
Chris Dunphy <chris.dunphy@fglsports.com>
2026-04-02 08:31:15 -0600

    Fix code smell in copilot plugin setup

--- lua/amn/plugins/copilot.lua
+++ lua/amn/plugins/copilot.lua
@@ -2,24 +2,23 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
-  opts = {
-    filetypes = {
-      markdown = false,
-      gitcommit = false,
-    },
-    suggestion = {
-      enabled = true,
-      auto_trigger = false,
-      keymap = {
-        accept = "<C-y>",
-        next = "<C-n>",
-        prev = "<C-p>",
-        dismiss = "<Esc>",
+  config = function()
+    require("copilot").setup({
+      filetypes = {
+        markdown = false,
+        gitcommit = false,
       },
-    },
-    panel = { enabled = false },
-  },
-  config = function(_, opts)
-    require("copilot").setup(opts)
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        keymap = {
+          accept = "<C-y>",
+          next = "<C-n>",
+          prev = "<C-p>",
+          dismiss = "<Esc>",
+        },
+      },
+      panel = { enabled = false },
+    })
   end,
 }
