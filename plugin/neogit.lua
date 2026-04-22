be4f9bb606c1f1a7680ddfca2fa6bc8d0ec5e5a9
Chris Dunphy <chris.dunphy@fglsports.com>
2026-04-22 08:49:49 -0600

    Try again to use replace for neogit
    I really hate tabs

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -6,6 +6,41 @@ neogit.setup({
   },
   graph_style = "unicode",
   process_spinner = true,
+  commit_editor = {
+    kind = "replace",
+  },
+  commit_select_view = {
+    kind = "replace",
+  },
+  commit_view = {
+    kind = "vsplit",
+  },
+  log_view = {
+    kind = "replace",
+  },
+  rebase_editor = {
+    kind = "auto",
+  },
+  reflog_view = {
+    kind = "replace",
+  },
+  merge_editor = {
+    kind = "auto",
+  },
+  preview_buffer = {
+    kind = "floating_console",
+  },
+  popup = {
+    kind = "split",
+  },
+  stash = {
+    kind = "replace",
+  },
+  refs_view = {
+    kind = "replace",
+  },
 })
 
-vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Neo[G]it [G]it" })
+vim.keymap.set("n", "<leader>gg", function()
+  neogit.open({ kind = "replace" })
+end, { desc = "Neo[G]it [G]it" })
