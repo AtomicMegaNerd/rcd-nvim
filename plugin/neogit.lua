8d41a5d68ab80ec8fd889eaa4aeabd23d567e64d
Chris Dunphy <chris@megaparsec.ca>
2026-04-22 15:04:55 -0600

    Fix neogit again

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -6,41 +6,6 @@ neogit.setup({
   },
   graph_style = "unicode",
   process_spinner = true,
-  commit_editor = {
-    kind = "floating",
-  },
-  commit_select_view = {
-    kind = "floating",
-  },
-  commit_view = {
-    kind = "vsplit",
-  },
-  log_view = {
-    kind = "floating",
-  },
-  rebase_editor = {
-    kind = "auto",
-  },
-  reflog_view = {
-    kind = "floating",
-  },
-  merge_editor = {
-    kind = "auto",
-  },
-  preview_buffer = {
-    kind = "floating_console",
-  },
-  popup = {
-    kind = "split",
-  },
-  stash = {
-    kind = "floating",
-  },
-  refs_view = {
-    kind = "floating",
-  },
 })
 
-vim.keymap.set("n", "<leader>gg", function()
-  neogit.open({ kind = "floating" })
-end, { desc = "Neo[G]it [G]it" })
+vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Neo[G]it [G]it" })
