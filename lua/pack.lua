5b0fa215bd900118c14e1272ad248b7bf60c8e1c
Chris Dunphy <chris@megaparsec.ca>
2026-04-29 20:57:00 -0600

    Bye bye plenary

--- lua/pack.lua
+++ lua/pack.lua
@@ -1,12 +1,10 @@
 vim.pack.add({
   { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-  { src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", name = "nvim-treesitter" },
   { src = "https://github.com/esmuellert/codediff.nvim", name = "codediff" },
   { src = "https://github.com/nvim-mini/mini.nvim", name = "mini" },
   { src = "https://github.com/ibhagwan/fzf-lua", name = "fzf-lua" },
   { src = "https://github.com/stevearc/conform.nvim", name = "conform" },
-  { src = "https://github.com/andythigpen/nvim-coverage", name = "coverage" },
   { src = "https://github.com/mfussenegger/nvim-lint", name = "lint" },
   { src = "https://github.com/NeogitOrg/neogit", name = "neogit" },
   { src = "https://github.com/stevearc/oil.nvim", name = "oil" },
