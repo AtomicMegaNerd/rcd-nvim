ed7c42892de8b7b0eb5bc4dbce49fab2c6bb6ed8
Chris Dunphy <chris@megaparsec.ca>
2026-06-06 09:00:44 -0600

    Fixes to autocmds
    This will be easier to use as I just need to define the overrides

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,75 +1,60 @@
 local amn_grp = vim.api.nvim_create_augroup("AtomicMegaNerdGroup", { clear = true })
 
--- Use tabs in these file types
+-- These languages are best using 2-spaces for indentation
+local two_space = {
+  "haskell",
+  "lua",
+  "terraform",
+  "nix",
+  "markdown",
+  "yaml",
+  "html",
+  "xml",
+  "css",
+  "json",
+  "scss",
+  "toml",
+  "javascript",
+  "javascriptreact",
+  "typescript",
+  "typescriptreact",
+}
+
+-- These languages use tabs instead of spaces
+local use_tabs = {
+  "go",
+  "make",
+}
+
+-- This autocmd controls indentation and tabs vs. spaces for
+-- all our file types
 vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal noexpandtab",
+  callback = function(args)
+    local ft = args.match
+    -- Default to spaces
+    vim.bo[args.buf].expandtab = true
+    -- Set indendtation to 2 for languages in two_space table
+    if vim.tbl_contains(two_space, ft) then
+      vim.bo[args.buf].tabstop = 2
+      vim.bo[args.buf].shiftwidth = 2
+    end
+    -- Use tabs for languages in the use_tabs table
+    if vim.tbl_contains(use_tabs, ft) then
+      vim.bo[args.buf].expandtab = false
+    end
+  end,
   group = amn_grp,
-  pattern = { "go", "make" },
+  pattern = { "*" },
 })
 
--- Use 2-space indentation with spaces
+-- For setting colorcolumn, Python is unique and has 88 cols standard, but we want to set
+-- it back to 100 for all other file types
 vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal tabstop=2 shiftwidth=2 expandtab",
+  callback = function(args)
+    vim.opt_local.colorcolumn = args.match == "python" and "88" or "100"
+  end,
   group = amn_grp,
-  pattern = {
-    "haskell",
-    "lua",
-    "terraform",
-    "nix",
-    "markdown",
-    "yaml",
-    "html",
-    "xml",
-    "css",
-    "json",
-    "scss",
-    "toml",
-    "javascript",
-    "javascriptreact",
-    "typescript",
-    "typescriptreact",
-  },
-})
-
--- Use spaces (default tabstop) in these file types
-vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal expandtab",
-  group = amn_grp,
-  pattern = {
-    "python",
-    "dockerfile",
-    "toml",
-    "xml",
-    "bash",
-    "sh",
-    "shell",
-  },
-})
-
--- The black Python formatter uses 88 characters as the line length
-vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal colorcolumn=88",
-  group = amn_grp,
-  pattern = { "python" },
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal colorcolumn=100",
-  group = amn_grp,
-  pattern = {
-    "markdown",
-    "lua",
-    "go",
-    "dockerfile",
-    "shell",
-    "sh",
-    "bash",
-    "javascript",
-    "typescript",
-    "yaml",
-    "json",
-    "toml",
-  },
+  pattern = { "*" },
 })
 
 -- Treat these files as shell scripts
@@ -77,11 +62,4 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   command = "setf sh",
   group = amn_grp,
   pattern = { "env.list", ".envrc", "*.env" },
-})
-
--- Disable spelling for these file types
-vim.api.nvim_create_autocmd("FileType", {
-  command = "setlocal nospell",
-  group = amn_grp,
-  pattern = { "yaml", "json", "toml", "xml" },
 })
