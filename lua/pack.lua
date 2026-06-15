ee5f56b45608dba3b3dd720013de3a7fb2f0ea31
Chris Dunphy <chris.dunphy@fglsports.com>
2026-06-15 08:07:42 -0600

    [main] Re-added co-pilot for now

--- lua/pack.lua
+++ lua/pack.lua
@@ -10,4 +10,5 @@ vim.pack.add({
   { src = "https://github.com/folke/todo-comments.nvim", name = "todo-comments" },
   { src = "https://github.com/saghen/blink.lib" },
   { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/zbirenbaum/copilot.lua" },
 })
---
+++ plugin/copilot.lua
@@ -0,0 +1,51 @@
+require("copilot").setup({
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+    layout = {
+      position = "bottom", -- | top | left | right | bottom |
+      ratio = 0.4,
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = false,
+    hide_during_completion = true,
+    debounce = 15,
+    trigger_on_accept = true,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+      toggle_auto_trigger = false,
+    },
+  },
+  workspace_folders = {},
+  disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
+})
+
+-- Don't conflict with blink.cmp
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
