a84b259025f38476fbe2357c1d1e9d13ebeb43c4
Chris Dunphy <chris.dunphy@fglsports.com>
2026-06-16 14:39:52 -0600

    [main] Re-add oxfmt for Markdown

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,6 +1,7 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
+    markdown = { "oxfmt" },
     go = { "golangci-lint" },
   },
   format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
