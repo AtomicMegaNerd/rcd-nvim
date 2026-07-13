828c41828cbe7c2b9f85a5d82964394ca5a7c7db
Chris Dunphy <chris.dunphy@fglsports.com>
2026-07-13 11:24:13 -0600

    [main] Catppuccin updates

--- plugin/catppuccin.lua
+++ plugin/catppuccin.lua
@@ -1,4 +1,11 @@
 require("catppuccin").setup({
   flavour = "macchiato",
+  no_italic = false,
+
+  integrations = {
+    grug_far = true,
+    overseer = true,
+    which_key = true,
+  },
 })
 vim.cmd.colorscheme("catppuccin-nvim")
