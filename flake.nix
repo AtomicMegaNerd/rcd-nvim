235e4c8a1c1ee492551e8e416cb34529abc9b7b8
Chris Dunphy <chris@megaparsec.ca>
2026-07-14 20:17:34 -0600

    Cleanup neovim flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     fenix.url = "github:nix-community/fenix";
   };
 
@@ -14,7 +10,6 @@
     {
       self,
       nixpkgs,
-      git-hooks,
       fenix,
     }:
     let
@@ -28,47 +23,6 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          pre-commit-check = git-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              oxfmt = {
-                enable = true;
-                name = "oxfmt";
-                entry = "oxfmt .";
-                pass_filenames = false;
-              };
-              stylua.enable = true;
-              # We need to tell lua-ls where the neovim runtime libraries are on
-              # Nix systems
-              lua-ls = {
-                enable = true;
-                settings.configuration = {
-                  runtime.version = "LuaJIT";
-                  workspace = {
-                    checkThirdParty = false;
-                    library = [
-                      "${pkgs.neovim-unwrapped}/share/nvim/runtime/lua"
-                    ];
-                  };
-                };
-              };
-              markdownlint.enable = true;
-              trim-trailing-whitespace.enable = true;
-              mixed-line-endings.enable = true;
-              end-of-file-fixer.enable = true;
-              nixfmt.enable = true;
-              flake-checker.enable = true;
-            };
-          };
-        }
-      );
-
       devShells = forAllSystems (system: {
         default =
           let
@@ -80,13 +34,7 @@
             inherit (self.checks.${system}.pre-commit-check) shellHook;
             packages = [
               pkgs.stylua
-              pkgs.lua-language-server
-              pkgs.markdownlint-cli2
-              pkgs.nixfmt
-              pkgs.nil
-              pkgs.oxfmt
               pkgs.libiconv
-              pkgs.biome
               # Added Rust to build blink.cmp native fuzzy library
               toolchain
               rust-analyzer
