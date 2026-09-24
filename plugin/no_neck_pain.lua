504ed53fc753e8f43244c3b2c4985ea265fe0d94
Chris Dunphy <chris.dunphy@fglsports.com>
2026-09-24 08:47:49 -0600

    [main] fixes for mini.starter

--- plugin/no_neck_pain.lua
+++ plugin/no_neck_pain.lua
@@ -1,7 +1,16 @@
 require("no-neck-pain").setup({
-  width = 112, -- Need to account for signcolumn and numbercolumn
+  width = 110, -- Need to account for signcolumn and numbercolumn
   minSideBufferWidth = 1,
   autocmds = {
-    enableOnVimEnter = true,
+    enableOnVimEnter = "safe",
+  },
+  mappings = {
+    enabled = true,
+  },
+  integrations = {
+    dashboard = {
+      enabled = true,
+      filetypes = { "ministarter" },
+    },
   },
 })
